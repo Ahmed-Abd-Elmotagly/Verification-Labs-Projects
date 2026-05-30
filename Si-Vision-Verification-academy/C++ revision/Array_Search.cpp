e5bc433541e1a7b2065559d852fdb942e21b6964
Ahmed Abd-Elmotagly <ahmedabdelmotagly18@gmail.com>
2026-05-31 00:12:01 +0300

    Update Array_Search.cpp

--- Si-Vision-Verification-academy/C++ revision/Array_Search.cpp
+++ Si-Vision-Verification-academy/C++ revision/Array_Search.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <stdint.h>
+#include <string.h>
+using namespace std;
 
+int search (const int arr[],int size,int item);
+
+int main () {
+    cout << "enter item to search in database " << "\n";
+    int database [5] = {4,8,9,5,7};
+    int look;
+    cin >> look;
+    search (database,5,look);
+}
+
+int search (const int arr[],int size,int item) {
+    for (int i=0;i<size;i++){
+        if (arr[i]==item) 
+        { 
+            cout << "found the item in index " << i << "\n";
+            return i;
+        } 
+        else continue;
+    }
+}
