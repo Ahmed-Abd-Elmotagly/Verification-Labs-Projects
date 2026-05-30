80d6e7f65199fc7433f28801b33525cf40ce3a43
Ahmed Abd-Elmotagly <ahmedabdelmotagly18@gmail.com>
2026-05-30 23:53:06 +0300

    Update salary_average.cpp

--- Si-Vision-Verification-academy/C++ revision/salary_average.cpp
+++ Si-Vision-Verification-academy/C++ revision/salary_average.cpp
@@ -1,1 +1,23 @@
+/*
+  revision on c++
+  program to ask user to enter 10 employee salaries and store them , then add 10% bonus for each employee and print the average 
+*/
 
+#include <iostream>
+#include <stdint.h>
+#include <string.h>
+using namespace std;
+
+int main () {
+    float base [10];
+    float bonus[10];
+    float sum = 0;
+    for (int i=0; i<10; i++) {
+        cout << "Enter employee salary number " << i+1 <<"\n";
+        cin >> base[i];
+        bonus[i] = base[i]*1.1;
+        sum += bonus[i];
+    }
+    cout << "average after added bonus is " << sum/10;
+
+}
