4f745997c286b2ae5262cdf570b7cca379725dd3
Ahmed Abd-Elmotagly <ahmedabdelmotagly18@gmail.com>
2026-05-31 03:19:45 +0300

    Create Matrix_elements_sum_diagonal_sum.cpp

---
+++ Si-Vision-Verification-academy/C++ revision/Matrix_elements_sum_diagonal_sum.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdint.h>
+#include <string.h>
+using namespace std;
+
+float matrix (float *elements,int rows,int cols);
+   
+   int main() {
+        cout << "enter matrix number of rows" << "\n" ;
+        int rows,cols;
+        cin >> rows;
+        cout << "enter matrix number of columns" << "\n" ;
+        cin >> cols;
+        float items [rows][cols];
+        cout << "enter elements, fill row then move to next" << "\n" ;
+        
+        for (int i=0; i<rows;i++) {
+            for (int j=0; j<cols;j++) {
+            cin >> items[i][j];
+            }
+        }
+
+        matrix ((float*)items,rows,cols);
+    }
+
+   float matrix (float *elements,int rows,int cols) {
+    
+    float sum = 0,diagonal = 0;
+    for (int i=0; i<rows;i++) {
+        for (int j=0; j<cols;j++) {
+            sum += elements[i*cols+j];
+            if (i==j) {
+                diagonal += elements[i*cols+j];
+            }
+        }
+    }
+    cout << "sum of all elements is " << sum << "\n";
+    cout << "sum of diagonal elements is " << diagonal << "\n";
+
+   }
